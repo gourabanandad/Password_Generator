5705956b83179b7b716aab4cf2d702dad3e47024
Gourabananda Datta <gourabanandadatta2003@gmail.com>
2025-07-11 14:32:58 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def generate_password(length: int = 15):
     return {"password": "".join(password)}
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
